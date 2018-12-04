a37b95cacde7194dc1e3e3e40d98a1496e323226
Macbook <macbook@MacBook-Pro-Macbook.local>
2018-12-04 12:27:35 +0300

    start new project

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  componentDidMount() {
+    console.log('before');
+    debugger;
+    console.log('after');
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Opening up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
